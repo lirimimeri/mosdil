a1ec251f193693835c8b47e0a4699a16ea80c8e4
lirimimeri <44481883+lirimimeri@users.noreply.github.com>
2020-09-18 21:40:36 +0200

    Updates

---
+++ precache-manifest.1f466b1ba261ba593e29021bc3c4f577.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "646012cc82547b4d8da2286190d9e535",
+    "url": "/mosdil/index.html"
+  },
+  {
+    "revision": "dc629888e97050096b8a",
+    "url": "/mosdil/static/css/main.0e7ed738.chunk.css"
+  },
+  {
+    "revision": "70ddd172a00ef756901e",
+    "url": "/mosdil/static/js/2.477a40f2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/mosdil/static/js/2.477a40f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc629888e97050096b8a",
+    "url": "/mosdil/static/js/main.96f28ca5.chunk.js"
+  },
+  {
+    "revision": "573ff9606c3ceb1fe733",
+    "url": "/mosdil/static/js/runtime-main.f821638b.js"
+  }
+]);
